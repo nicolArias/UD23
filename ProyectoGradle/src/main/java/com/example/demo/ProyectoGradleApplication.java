2de4f98a11d0c14c4bcf21e1363865510a341ec2
Nicol Arias <nicolariaslebro@gmail.com>
2021-03-13 16:15:07 +0100

    Unidad 23, ejercicio de 'Hola Mundo' con gradle.

---
+++ ProyectoGradle/src/main/java/com/example/demo/ProyectoGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGradleApplication.class, args);
+	}
+
+}
---
+++ ProyectoGradle/src/main/java/com/example/demo/controller/Controlador.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controlador {
+	
+	@RequestMapping("/")
+	String hola() {
+		return "Hola Mundo! Proyecto gradle";
+	}
+
+}
