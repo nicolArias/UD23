e71b0211698d610deb0af57c1e08a275270e8545
Nicol Arias <nicolariaslebro@gmail.com>
2021-03-13 16:21:28 +0100

    Unidad 23 'Hola Mundo' proyecto maven.

---
+++ ProyectoMaven/src/main/java/com/example/demo/ProyectoMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoMavenApplication.class, args);
+	}
+
+}
