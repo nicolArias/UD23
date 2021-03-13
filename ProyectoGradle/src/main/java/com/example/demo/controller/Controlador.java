1fe3ed9cd33c7da5d715b40ccc791c7c65b235b6
Nicol Arias <nicolariaslebro@gmail.com>
2021-03-13 18:11:41 +0100

    Ajuste en el controlador.

--- ProyectoGradle/src/main/java/com/example/demo/controller/Controlador.java
+++ ProyectoGradle/src/main/java/com/example/demo/controller/Controlador.java
@@ -1,12 +1,12 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controlador {
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	String hola() {
 		return "Hola Mundo! Proyecto gradle";
 	}
